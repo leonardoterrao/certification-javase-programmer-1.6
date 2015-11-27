58dcc1546f8a3dd63be3108a2f53758dd72003f3
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2015-11-26 22:32:44 -0200

    Exercises Section 7 Fundamentrals

---
+++ src/practice/exams/java/chapter4/question7/a/ClazzFinalTests.java
@@ -0,0 +1,22 @@
+package practice.exams.java.chapter4.question7.a;
+
+public class ClazzFinalTests {
+
+	final int primitiveFinal;
+
+	final Long variableFinal;
+	
+	public ClazzFinalTests() {
+		primitiveFinal = 50;
+		variableFinal = 5055L;
+	
+		
+		System.out.println(primitiveFinal);
+		setNewVale(primitiveFinal);
+	}
+	
+	private void setNewVale(int primitiveFinal2) {
+		
+	}
+
+}
