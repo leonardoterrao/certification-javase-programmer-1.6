baae101c7f336e0699fbb873fdeb52f48a31cc9f
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2015-11-26 22:23:38 -0200

    Exercises Section 6 Collections and Generics

--- src/practice/exams/java/chapter4/question6/k/Clazz.java
+++ src/practice/exams/java/chapter4/question6/k/Clazz.java
@@ -1,11 +1,21 @@
 package practice.exams.java.chapter4.question6.k;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Clazz {
 
 	public static void main(String[] args) {
+		ArrayList arrayList = methodAnyType(new ArrayList());
+		for (Object object : arrayList) {
+			System.out.println(object.toString());
+		}
+	}
 
-		
-		
+	private static ArrayList methodAnyType(Object object) {
+		ArrayList list = (ArrayList) object;
+		list.add(object);
+		return list;
 	}
 	
 }
