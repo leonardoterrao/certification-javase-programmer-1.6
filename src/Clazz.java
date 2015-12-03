be2b5cdbbfd0af52d0214da9a87dc0d2dec42491
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2015-12-03 00:39:33 -0200

    Exercices test killer

--- src/Clazz.java
+++ src/Clazz.java
@@ -1,27 +1,15 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
 public class Clazz {
 
 	public static void main(String[] args) {
-		
-//		String s = "123456789";
-//		s = (s-"123").replace(1,3,"24") - "89";
-		
-		StringBuffer s1 = new StringBuffer("123456789");
-		s1.delete(0,3).replace(1,3,"24").delete(4,6);
-		
-//		StringBuffer s2 = new StringBuffer("123456789");
-//		s2.substring(3,6).delete(1,3).insert(1, "24");
-//		
-//		StringBuilder s3 = new StringBuilder("123456789");
-//		s3.substring(3,6).delete(1,2).insert(1, "24");
-//		
-		StringBuilder s4 = new StringBuilder("123456789");
-		s4.delete(0,3).delete(1,3).delete(2,5).insert(1, "24");
-		
-		System.out.println(s1);
-		System.out.println(s4);
+
+	}
+
+	public static void test(String str) {
+//		int check = 4;
+//		if (check = str.length()) {
+//			System.out.print(str.charAt(check -= 1) + ", ");
+//		} else {
+//			System.out.print(str.charAt(0) + ", ");
+//		}
 	}
 }
